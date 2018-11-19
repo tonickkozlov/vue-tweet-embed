74e71d57978e84a705114de2e04ae3308c860bdb
sayaka0301 <you@example.com>
2018-11-19 15:13:52 +0900

    addScriptPromise初期値変更

--- core/index.js
+++ core/index.js
@@ -1,7 +1,7 @@
-let addScriptPromise = null
+let addScriptPromise = 0;
 
 /** Adds proviced script to the page, once **/
-function addPlatformScript (src) {
+function addPlatformScript(src) {
     if (!addScriptPromise) {
         const s = document.createElement('script')
         s.setAttribute('src', src)
@@ -29,36 +29,48 @@ const defaultProps = {
 /** Basic function used to mount Twitter component */
 const twitterEmbedComponent = (me) => {
     return {
-        data () {
+        data() {
             return {
                 isLoaded: false,
                 isAvailable: false
             }
         },
         props: Object.assign({}, defaultProps, me.props),
-        mounted () {
-            let params = (this.sourceType) ? { sourceType: this.sourceType, screenName: this.id } : this.id
+        mounted() {
+            let params = (this.sourceType) ? {
+                sourceType: this.sourceType,
+                screenName: this.id
+            } : this.id
 
             Promise.resolve(window.twttr ? window.twttr : addPlatformScript('//platform.twitter.com/widgets.js'))
-            .then(twttr => me.embedComponent(twttr, params, this.$el, this.options))
-            .then(data => {
-                this.isAvailable = (data !== undefined)
-                this.isLoaded = true
-            })
+                .then(twttr => me.embedComponent(twttr, params, this.$el, this.options))
+                .then(data => {
+                    this.isAvailable = (data !== undefined)
+                    this.isLoaded = true
+                })
         },
-        render (h) {
+        render(h) {
             if (this.isLoaded && this.isAvailable) {
-                return h('div', { class: this.$props.widgetClass })
+                return h('div', {
+                    class: this.$props.widgetClass
+                })
             }
 
             if (this.isLoaded && !this.isAvailable && this.$props.errorMessage) {
-                const $errorMsg = h('div', { class: this.$props.errorMessageClass }, this.$props.errorMessage)
+                const $errorMsg = h('div', {
+                    class: this.$props.errorMessageClass
+                }, this.$props.errorMessage)
                 return h('div', [$errorMsg])
             }
 
-            return h('div', { class: this.$props.widgetClass }, this.$slots.default)
+            return h('div', {
+                class: this.$props.widgetClass
+            }, this.$slots.default)
         }
     }
 }
 
-module.exports = { addPlatformScript, twitterEmbedComponent }
+module.exports = {
+    addPlatformScript,
+    twitterEmbedComponent
+}
