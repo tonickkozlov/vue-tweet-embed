41e85bba64b17a03341aad13c8bf675dc635451c
Anton Kozlov <tonickkozlov@me.com>
2019-11-06 22:05:34 +0000

    [feat] added puppeteer test for Tweet, updated ava

---
+++ examples/single-tweet.js
@@ -0,0 +1,10 @@
+const Vue = require('vue/dist/vue.js')
+const Tweet = require('../dist/tweet').default
+
+new Vue({
+  el: '#app',
+  template: `<Tweet :id="'1129378242283098112'"></Tweet>`,
+  components: {
+    Tweet,
+  }
+})
