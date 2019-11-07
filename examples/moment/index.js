7ee61c24407ca4a7074410bb236cb27ec0a17ff5
Anton Kozlov <tonickkozlov@me.com>
2019-11-07 23:41:52 +0000

    [feat] added Moment example

---
+++ examples/moment/index.js
@@ -0,0 +1,10 @@
+const Vue = require('vue/dist/vue.js')
+const Moment = require('../../dist/moment').default
+
+new Vue({
+  el: '#app',
+  template: `<Moment :id="'784457497809682433'"></Moment>`,
+  components: {
+    Moment,
+  }
+})
