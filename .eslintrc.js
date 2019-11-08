3dcb33293a893989915acb5df80cdf3a1a261594
Anton Kozlov <tonickkozlov@me.com>
2019-11-08 14:45:34 +0000

    [eslint] Updated eslint config, fixed linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,23 @@
 module.exports = {
-    "extends": "standard",
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
     "plugins": [
-        "standard",
-        "promise"
+        "vue"
     ],
     "rules": {
-        "indent": [2, 4]
     }
 };
